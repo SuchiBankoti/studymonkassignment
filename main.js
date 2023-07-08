85d24aa26e0ba9caa71663ee9e37c308bc1f6a90
suchi <suchibankoti95@gmail.com>
2023-07-08 10:38:55 +0530

    add javascript to homepage

--- main.js
+++ main.js
@@ -56,3 +56,58 @@ window.addEventListener("scroll", () => {
     back.style.opacity = "1";
   }
 });
+
+const hireData = [
+  {
+    title: "Offer Management",
+    text: "Streamline your offer process with custom offer templates, approval flows, and built-in eSignatures. Get offers out to candidates as quickly as possible, so you can keep up with your hiring goals.",
+    img: "https://assets-global.website-files.com/6127d83f257132e4fe0bddc6/621cf254e8ae4fb706887c68_breezyhr-offer-management.jpg",
+  },
+  {
+    title: "Background Screening",
+    text: "Get pre-employment screening done in days, not weeks. With integrations from top providers, Breezy makes it fast and easy to run background checks you can trust.",
+    img: "https://assets-global.website-files.com/6127d83f257132e4fe0bddc6/621cf296c2a0dc0d7a51c902_breezyhr-background-screening-p-800.jpeg",
+  },
+  {
+    title: "HRIS Integration",
+    text: "Breezy integrates with the most popular tools in recruiting and HR. That means you can easily move new hires into the HR system that works for you.",
+    img: "https://assets-global.website-files.com/6127d83f257132e4fe0bddc6/642f03526a398cc5374850e1_breezyhr-HRIS-integrations-p-800.jpg",
+  },
+  {
+    title: "Compliance",
+    text: "Our suite of tools and features help automate and simplify the process of staying General Data Protection Regulation (GDPR) compliant.",
+    img: "https://assets-global.website-files.com/6127d83f257132e4fe0bddc6/621cf2e8d3876c6c53895ffa_breezyhr-compliance-p-800.jpeg",
+  },
+];
+
+const listDetails = document.getElementById("listDetails");
+const list = document.getElementsByClassName("section3-li-items");
+Array.from(list).forEach((li, i) => {
+  li.addEventListener("click", () => addListDetails(i));
+  li.addEventListener("click", addStyleTocurrentList);
+});
+function addStyleTocurrentList(e) {
+  Array.from(list).forEach((li) => {
+    li.classList.remove("selected-list");
+  });
+  e.target.classList.add("selected-list");
+}
+function addListDetails(id) {
+  while (listDetails.firstChild) {
+    listDetails.removeChild(listDetails.firstChild);
+  }
+  const data = hireData[id];
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
+  const img = document.createElement("img");
+  h2.classList.add("section3-sub-title");
+  p.classList.add("section3-sub-text");
+  img.classList.add("section3-sub-img");
+  h2.textContent = data.title;
+  p.textContent = data.text;
+  img.src = data.img;
+  img.alt = "picture";
+  listDetails.appendChild(h2);
+  listDetails.appendChild(p);
+  listDetails.appendChild(img);
+}
